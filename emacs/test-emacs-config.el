c5963b4fe7ec4d834cd5258d105bb9e0b89f9de7
oEfterdal <oystein@efterdal.no>
2026-01-06 01:25:38 +0100

    Synk

--- emacs/test-emacs-config.el
+++
@@ -1,4 +0,0 @@
-(setq debug-on-error t)
-(setq user-emacs-directory (expand-file-name "~/.emacs.d/"))
-(load (expand-file-name "~/.config/emacs/init.el" nil 'nomessage))
-(message "Config loaded successfully")
